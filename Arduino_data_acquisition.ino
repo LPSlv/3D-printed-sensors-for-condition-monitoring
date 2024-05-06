3b9daf9860fc06c77238ddc6ee33262f2cc00cb9
LPSlv <125740111+LPSlv@users.noreply.github.com>
2024-05-06 19:35:09 +0300

    Improved reliability

--- Arduino_data_acquisition.ino
+++ Arduino_data_acquisition.ino
@@ -1,26 +1,21 @@
 const int analogPin = A0;
-const unsigned long samplingInterval = 199;  // Sampling interval in microseconds
+const unsigned long samplingInterval = 50;  // Sampling interval in microseconds
 
 void setup() {
-  Serial.begin(115200);  // Set baud rate to 115200
+  analogReference(INTERNAL);
+  Serial.begin(115200);
 }
 
 void loop() {
   static unsigned long lastSampleTime = 0;
   unsigned long currentTime = micros();
 
-  // Check if it's time to take a new sample
   if (currentTime - lastSampleTime >= samplingInterval) {
-    // Read analog value from A0
     int sensorValue = analogRead(analogPin);
     
-    // Send current time as raw binary data
-    Serial.write((uint8_t*)&currentTime, 4);  // Sending 4 bytes
-    
-    // Send analog value as raw binary data
-    Serial.write((uint8_t*)&sensorValue, 2);  // Sending 2 bytes
+    // Send sensor value as 2 bytes
+    Serial.write((uint8_t*)&sensorValue, 2);
 
-    // Update last sample time
     lastSampleTime = currentTime;
   }
 }
